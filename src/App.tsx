de9b62cbee5d96e97795e040554bee55489e1cad
Nasos <nikolaykalayanov01@gmail.com>
2022-10-13 20:16:34 +0300

    upd 1.5

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,16 @@ const Title = () => <h1>Hello React</h1>
 const List = () => {
     let a = 10
     return(
-        <ul>
-            <li>list item {a + 1}</li>
-            <li>list item 2</li>
-            <li>list item 3</li>
-        </ul>
+        <React.Fragment>
+            <ul>
+                <li>list item {a + 1}</li>
+                <li>list item 2</li>
+                <li>list item 3</li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit minima temporibus soluta ipsam hic corrupti nisi facere cumque. Consectetur saepe nemo sapiente veritatis explicabo commodi modi enim. Dolor, corporis fuga!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit minima temporibus soluta ipsam hic corrupti nisi facere cumque. Consectetur saepe nemo sapiente veritatis explicabo commodi modi enim. Dolor, corporis fuga!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit minima temporibus soluta ipsam hic corrupti nisi facere cumque. Consectetur saepe nemo sapiente veritatis explicabo commodi modi enim. Dolor, corporis fuga!</p>
+        </React.Fragment>
     )
 }
 
