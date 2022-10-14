d404774298a0bb3054ad528477eef20aaea63668
Nasos <nikolaykalayanov01@gmail.com>
2022-10-14 20:24:06 +0300

    upd 2.2

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <div>Header</div>
+    <>
+        <AppBar position="static">
+            <Container>
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Fake shop
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+            </Container>
+        </AppBar>
+    </>
   )
 }
 
