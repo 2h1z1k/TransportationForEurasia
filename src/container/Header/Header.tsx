78abbfe34fcddce5857789170d15466c52f52e18
Nasos <nikolaykalayanov01@gmail.com>
2022-10-18 20:47:56 +0300

    upd 3.8

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,43 +1,48 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 import Menu from 'components/Menu/Menu'
 import CartHeader from 'components/Cart/CartHeader'
+import './Header.scss'
 
 type Props = {}
 
 const Header = (props: Props) => {
-  return (
-    <>
-        <AppBar position="static">
-            <Container>
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Fake shop
-                </Typography>
-                <Menu/>
-                <CartHeader/>
-            </Toolbar>
-            </Container>
-        </AppBar>
-    </>
-  )
+    return (
+        <>
+            <AppBar position="static" className="app-bar">
+                <Container>
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Fake shop
+                        </Typography>
+                        <Menu />
+                        <CartHeader />
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
+    )
 }
 
 export default Header
